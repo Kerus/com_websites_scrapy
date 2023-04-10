138aa9d08536230b37e27fa5cab4f16779a35e2c
KerusWSL <gibiskusus@gmail.com>
2023-04-10 16:02:29 +0200

    fix: add m-ware to settings and fix path filter

--- company_parser/company_parser/spiders/company_websites_spider.py
+++ company_parser/company_parser/spiders/company_websites_spider.py
@@ -12,26 +12,36 @@ class CompanyWebSiteSpider(scrapy.Spider):
     custom_settings = {
         "DOWNLOADER_MIDDLEWARES": {
             'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-            'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400
+            'company_parser.middlewares.ProxyPlayWrightMiddleware': 500,
         },
         'REDIRECT_ENABLED': False,
+        'USER_AGENT': None,
         'TWISTED_REACTOR': "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
         'DOWNLOAD_HANDLERS': {
             "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
             "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-        }
+        },
+        'PLAYWRIGHT_BROWSER_TYPE': "firefox",
+        'PROXY_LIST': [
+            'http://200.52.148.10:999',
+            'http://90.84.17.133:3128',
+            'http://103.148.39.42:84',
+            'http://84.254.0.86:32650'
+        ]
     }
     
     def start_requests(self):
         fname = os.path.join(os.getcwd(), 'full_sample.xlsx')
         companies_data = pd.read_excel(fname)
         companies_data = companies_data.head(1)
-        keywords = ["official website", "homepage"]
+        keywords = ["official", "website", "homepage"]
         for i, company in companies_data.iterrows():
             company_name = company["Account Name"]
             query = f"{'+'.join(company_name.split())}+" + "+".join(keywords)
             url = f"https://duckduckgo.com/?q={query}"
-            yield scrapy.Request(url=url, callback=self.parse, meta={'name': company_name, 'playwright': True})
+            ref = {'Referer': 'https://duckduckgo.com',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
+            yield scrapy.Request(url=url, callback=self.parse, headers=ref, meta={'name': company_name})
 
     def parse(self, response):
 
@@ -43,7 +53,7 @@ class CompanyWebSiteSpider(scrapy.Spider):
             links = response.xpath('//div[@id="links"]//h2/a[contains(@href,"http")]/@href').getall()
             stop_words_filter = filter(lambda x: all(stop not in x for stop in stop_words), links)
             unquote_filter = filter(lambda x: urllib.parse.unquote(x), stop_words_filter)
-            path_filter = filter(lambda x: len(urllib.parse.urlparse(x).path.split('/')) < 3, unquote_filter)
+            path_filter = filter(lambda x: len(set(urllib.parse.urlparse(x).path.split('/'))) < 3, unquote_filter)
             links = list(path_filter)
             if len(links) > 0:
                 website = links[0]
